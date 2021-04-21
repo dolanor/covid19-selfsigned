8271d171ff43245a3403a553994420a70ada290e
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2021-04-22 01:53:44 +0200

    use gioui

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/dolanor/covid19-selfsigned
 
 go 1.16
 
-require github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+require (
+	gioui.org v0.0.0-20210421151739-2296c80d288b
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	golang.org/x/image v0.0.0-20200618115811-c13761719519
+)
--- main.go
+++ main.go
@@ -1,38 +1,85 @@
 package main
 
 import (
-	"bytes"
-	"image/png"
+	"image"
+	"log"
 	"os"
 
+	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/paint"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
 	"github.com/skip2/go-qrcode"
 )
 
 func main() {
-	err := run()
-	if err != nil {
-		panic(err)
-	}
+	go func() {
+		err := run()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	app.Main()
 }
+
 func run() error {
 	code, err := qrcode.New(AttestationEncode(), qrcode.Highest)
 	if err != nil {
 		return err
 	}
-	img := code.Image(-1)
+	img := code.Image(-5)
+	_ = img
+	gui(img)
+	return nil
+}
 
-	f, err := os.Create("lol.png")
-	if err != nil {
-		return err
+func gui(img image.Image) {
+	w := app.NewWindow()
+	if err := loop(w, "Attestation Dérogatoire COVID19", img); err != nil {
+		log.Fatal(err)
 	}
-	defer f.Close()
+	os.Exit(0)
+}
 
-	var buf bytes.Buffer
-	_ = buf
-	err = png.Encode(f, img)
-	if err != nil {
-		return err
+type C = layout.Context
+type D = layout.Dimensions
+
+func loop(w *app.Window, title string, img image.Image) error {
+	th := material.NewTheme(gofont.Collection())
+	var ops op.Ops
+
+	for e := range w.Events() {
+		switch e := e.(type) {
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, e)
+			in := layout.UniformInset(unit.Dp(8))
+			layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+				layout.Rigid(func(gtx C) D {
+					return in.Layout(gtx, func(gtx C) D {
+						t := material.Body1(th, title)
+						return t.Layout(gtx)
+					})
+				}),
+				layout.Flexed(1, func(gtx C) D {
+					return in.Layout(gtx, func(gtx C) D {
+						qrOp := paint.NewImageOp(img)
+						imgWidget := widget.Image{Src: qrOp, Fit: widget.ScaleDown}
+						return imgWidget.Layout(gtx)
+					})
+				}),
+			)
+
+			e.Frame(gtx.Ops)
+		case system.DestroyEvent:
+			return e.Err
+		}
 	}
+
 	return nil
 }
 
